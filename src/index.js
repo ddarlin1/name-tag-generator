b7ce66cda415f879eac76268ef00b6f10803be17
darling_d <ddarling212@gmail.com>
2021-12-07 18:38:23 -0500

    Changed components from functional to stateful

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
-import { namesData } from "./data.js";
 import "./styles.css";
 
-const renderNameTag = (name) => <NameTag key={name.id} name={name.person} />;
+const renderNameTag = (name) => <NameTag name={name} key={name} />;
 
-const App = () => {
-  const NameTagElements = namesData.map(renderNameTag);
-  return (
-    <div className="App">
-      <h1>Name Tag Generator</h1>
-      {NameTagElements}
-    </div>
-  );
-};
+class App extends Component {
+  state = { names: ["Dani", "Quinn", "Fern", "Flo", "Magda", "Daniel"] };
+  render() {
+    const NameTagElements = this.state.names.map(renderNameTag);
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        {NameTagElements}
+      </div>
+    );
+  }
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
